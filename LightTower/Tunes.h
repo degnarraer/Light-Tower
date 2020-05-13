0bdfaba6bf598e0641e4333a3a658afb80aca446
Rob Shockency <degnarraer@yahoo.com>
2020-05-12 21:47:42 -0500

    Tunes Clean Up

--- LightTower/Tunes.h
+++ LightTower/Tunes.h
@@ -10,7 +10,7 @@ enum MicType
   MIC_TYPE_AUTOGAIN,
   MIC_TYPE_FIXEDGAIN
 };
-static MicType micType = MicType::MIC_TYPE_FIXEDGAIN;
+static const MicType micType = MicType::MIC_TYPE_FIXEDGAIN;
 
 //Run Fixed Data Tables instead of microphone
 static bool   testMode = false;
@@ -23,32 +23,36 @@ static bool   debugPlotFFT = false;
 static int    debugLevel = 1;
 
 // LED SETUP
-#define NUMLEDS 60
-#define NUMSTRIPS 4
-#define DATA_PIN_STRIP1 4
-#define DATA_PIN_STRIP2 5
-#define DATA_PIN_STRIP3 6
-#define DATA_PIN_STRIP4 7
+const unsigned int NUMLEDS = 60;
+const unsigned int NUMSTRIPS = 4;
+const unsigned int DATA_PIN_STRIP1 = 4;  //STRIP1 PIN ASSIGNMENT
+const unsigned int DATA_PIN_STRIP2 = 5;  //STRIP2 PIN ASSIGNMENT
+const unsigned int DATA_PIN_STRIP3 = 6;  //STRIP3 PIN ASSIGNMENT
+const unsigned int DATA_PIN_STRIP4 = 7;  //STRIP4 PIN ASSIGNMENT
 
 // VISUALIZATION TUNES
-#define NUMSPRITES 8
-#define MAX_DISPLAYED_FREQ 10000.0
-#define NUMBER_OF_TICK_TIMERS 10
-#define NUMBER_OF_LAYERS 10
-#define NUMBER_OF_FADE_CONTROLLERS 10
+const float MAX_DISPLAYED_FREQ = 10000.0;
+const unsigned int NUMBER_OF_TICK_TIMERS = 10;
+const unsigned int NUMBER_OF_LAYERS = 10;
 
 //FFT Tunes
 const int FFT_MAX = 128;
 const unsigned int SAMPLE_RATE = 26000;
 const int SAMPLE_TIME_US = 1000000.0/SAMPLE_RATE;
+const unsigned int BIN_SAVE_LENGTH = 60;
 
-// Sampler
-const unsigned int NUM_CHANNELS = 3;
+//Trigger Level
+const float SILENCE_THRESHOLD = 0.025;
+static float triggerLevelGain = 1.0;
+
+// Sampler Tunes
 /* ch7:A0 ch6:A1 ch5:A2 ch4:A3 ch3:A4 ch2:A5 ch1:A6 ch0:A7 */
 #define ADC_CHANNELS ADC_CHER_CH7 | ADC_CHER_CH5 | ADC_CHER_CH4
+#define ADC_RESOLUTION 12
+const unsigned int NUM_CHANNELS = 3;
 const unsigned int CHANNEL_SIZE = FFT_MAX;
 const unsigned int BUFFER_SIZE = CHANNEL_SIZE*NUM_CHANNELS;
-const unsigned int NUMBER_OF_BUFFERS = 20;  /// Make this 3 or greater
+const unsigned int NUMBER_OF_BUFFERS = 3;  /// Make this 3 or greater
 
 //Sound Detection
 const float   SOUND_DETECT_THRESHOLD = 0.05;
@@ -57,15 +61,6 @@ const int     silenceDetectedThreshold = 0.1*silenceIntegratorMax;
 const int     soundDetectedThreshold = 0.9*silenceIntegratorMax;
 const int     soundAdder = 1000;
 const int     silenceSubtractor = -100;
-
-//Other Tunes
-#define ADC_RESOLUTION 12
-const float Vin = 3.3;
-const int BIN_SAVE_LENGTH = 60;
-
-//Trigger Level
-const float SILENCE_THRESHOLD = 0.025;
-static float triggerLevelGain = 1.0;
 
 //CALCULATED TUNES
 const int FFT_M = (int)log2(FFT_MAX);
