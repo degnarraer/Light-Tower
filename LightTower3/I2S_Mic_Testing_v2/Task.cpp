9baa87ebb63f9759ade694aa33aced896fc060fe
Rob Shockency <degnarraer@yahoo.com>
2021-11-16 23:15:27 -0600

    ReAdding back in the initial commit for Light Tower v3

---
+++ LightTower3/I2S_Mic_Testing_v2/Task.cpp
@@ -0,0 +1,86 @@
+/*
+    Light Tower by Rob Shockency
+    Copyright (C) 2021 Rob Shockency degnarraer@yahoo.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version of the License, or
+    (at your option) any later version. 3
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "Task.h"
+#include <typeinfo>
+
+void Task::AddTask(Task &task)
+{
+  m_Scheduler.AddTask(task);
+}
+void Task::AddTasks(LinkedList<Task*> &tasks)
+{
+  m_Scheduler.AddTasks(tasks);
+}
+bool Task::RemoveTask(Task &task)
+{
+  m_Scheduler.RemoveTask(task);
+}
+void Task::RunScheduler()
+{
+  m_Scheduler.RunScheduler(); 
+}
+void TaskScheduler::RunScheduler()
+{
+  for(int t = 0; t < m_MyTasks.size(); ++t)
+  {
+    Task *aTask = m_MyTasks.get(t);
+    aTask->RunScheduler();
+    if(true==aTask->CanRunMyTask())
+    {
+      aTask->RunMyTask();
+    }
+  }
+}
+void TaskScheduler::AddTask(Task &task)
+{
+  m_MyTasks.add(&task);
+  if(false == task.GetIsSetup())
+  {
+    task.Setup();
+    task.SetIsSetup(true);
+  }
+}
+void TaskScheduler::AddTasks(LinkedList<Task*> &tasks)
+{
+  for(int t = 0; t < tasks.size(); ++t)
+  {
+    m_MyTasks.add(tasks.get(t));
+  }
+}
+bool TaskScheduler::RemoveTask(Task &task)
+{
+  bool taskFound = false;
+  for(int i = 0; i < m_MyTasks.size(); ++i)
+  {
+    if(m_MyTasks.get(i) == &task)
+    {
+      taskFound = true;
+      m_MyTasks.remove(i);
+      break;
+    }
+  }
+  if(true == taskFound)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
