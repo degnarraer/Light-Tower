c3ae0f478edad2396be65dd804ea857c921013a7
Rob Shockency <degnarraer@yahoo.com>
2023-03-05 11:03:30 -0600

    adding cache control to web server

--- LightTower3/LED_Controller/Web_Interface/Web_Interface.ino
+++ LightTower3/LED_Controller/Web_Interface/Web_Interface.ino
@@ -67,7 +67,7 @@ void InitWebServer()
   {
     request->send(SPIFFS, "/index.html", "text/html");
   });
-  MyWebServer.serveStatic("/", SPIFFS, "/");
+  MyWebServer.serveStatic("/", SPIFFS, "/").setCacheControl("max-age31536000");;
 }
 
 // Start the web server
@@ -125,6 +125,7 @@ void setup(){
 
 void loop()
 {
+  //MyWebSocket.cleanupClients();
 }
 
 void SPI_RX_TaskLoop(void * parameter)
